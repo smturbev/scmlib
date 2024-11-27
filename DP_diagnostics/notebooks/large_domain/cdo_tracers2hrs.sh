adaeb2fa6338a3fc2dacf7e7ac642d675f87c544
Sami Turbeville <smturbev@uw.edu>
2024-11-27 10:24:38 -0700

    testing large domain BCU threshold in default branch

--- DP_diagnostics/notebooks/large_domain/cdo_tracers2hrs.sh
+++ DP_diagnostics/notebooks/large_domain/cdo_tracers2hrs.sh
@@ -1,31 +1,32 @@
 #!/bin/bash
-#PBS -N zTraclp
+#PBS -N zTracBranch
 #PBS -A UWAS0108
-#PBS -l walltime=00:15:00
+#PBS -l walltime=00:30:00
 #PBS -q develop
 #PBS -j oe
 #PBS -k eod
 #PBS -m be
 #PBS -M smturbev@uw.edu
-#PBS -l select=1:ncpus=8:mpiprocs=8
+#PBS -l select=1:ncpus=8:mpiprocs=87
 
 module load cdo
  
-run=aa_lpfrz
+run=default_branch
 run_dir=/glade/derecho/scratch/sturbeville/DPSCREAM_simulations/dpscream_rce_large_3km_${run}/run/
-ds_file=$run_dir/dpscream_rce_large_3km_${run}.eam.h0.2000-01-01-00000.nc
+ds_file=$run_dir/dpscream_rce_large_3km_${run}.eam.h0.2000-02-20-21600.nc
+ds5day_file=$run_dir/dpscream_rce_large_3km_${run}_h0_last5days.nc
 
-cdo -seltimestep,-20/-1 $ds_file $run_dir/dpscream_rce_large_3km_${run}_h0_last5days.nc
+cdo -seltimestep,-20/-1 $ds_file $ds5day_file
 
-# cdo -fldmean -timmean -seltimestep,-20/-1 -selvar,Z3 $ds_file $run_dir/dpscream_rce_large_3km_${run}_Z3_mean.nc
+cdo -fldmean -timmean -seltimestep,-20/-1 -selvar,Z3 $ds_file $run_dir/dpscream_rce_large_3km_${run}_Z3_mean.nc
 
-# tracer=BCU
-# tracer_file=$run_dir/dpscream_rce_large_3km_${run}_${tracer}_hrs.nc
-# cdo -mulc,-1 -ln -selvar,$tracer $ds_file $tracer_file # -seltimestep,-20/-1 
+tracer=BCU
+tracer_file=$run_dir/dpscream_rce_large_3km_${run}_${tracer}_hrs.nc
+cdo -mulc,-1 -ln -selvar,$tracer $ds5day_file $tracer_file # -seltimestep,-20/-1 
 
-# tracer=NUC
-# tracer_file=$run_dir/dpscream_rce_large_3km_${run}_${tracer}_hrs.nc
-# cdo -mulc,-1 -ln -selvar,$tracer $ds_file $tracer_file # -seltimestep,-20/-1
+tracer=NUC
+tracer_file=$run_dir/dpscream_rce_large_3km_${run}_${tracer}_hrs.nc
+cdo -mulc,-1 -ln -selvar,$tracer $ds5day_file $tracer_file # -seltimestep,-20/-1
 
 # tracer=W_NUC
 # tracer_file=$run_dir/dpscream_rce_large_3km_${run}_${tracer}_hrs.nc
