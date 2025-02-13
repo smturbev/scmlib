67dc39c1cbf34a0c5fa5dad4da2b39330c51d6ba
Sami Turbeville <smturbev@uw.edu>
2025-02-13 09:35:21 -0700

    post-AMS25 mtg updates

--- DP_diagnostics/notebooks/large_domain/cdo_tracers2hrs.sh
+++ DP_diagnostics/notebooks/large_domain/cdo_tracers2hrs.sh
@@ -11,31 +11,31 @@
 
 module load cdo
  
-run=lpls300k_wbranch
+run=lpls304k
 run_dir=/glade/derecho/scratch/sturbeville/DPSCREAM_simulations/dpscream_rce_large_3km_${run}/run/
 ds_file=$run_dir/dpscream_rce_large_3km_${run}.eam.h0.2000-02-20-10800.nc
-ds5day_file=$ds_file
-# ds5day_file=$run_dir/dpscream_rce_large_3km_${run}_h0_last5days.nc
+# ds5day_file=$ds_file
+ds5day_file=$run_dir/dpscream_rce_large_3km_${run}_h0_last5days.nc
 tracer_file=$run_dir/dpscream_rce_large_3km_${run}
 
-# cdo -seltimestep,-20/-1 $ds_file $ds5day_file
+cdo -seltimestep,-20/-1 $ds_file $ds5day_file
 
-cdo -fldmean -timmean -seltimestep,-20/-1 -selvar,Z3 $ds_file $run_dir/dpscream_rce_large_3km_${run}_Z3_mean.nc
+# cdo -fldmean -timmean -seltimestep,-20/-1 -selvar,Z3 $ds_file $run_dir/dpscream_rce_large_3km_${run}_Z3_mean.nc
 
-tracer=BCU
-tracer_file=$run_dir/dpscream_rce_large_3km_${run}_${tracer}_hrs.nc
-cdo -mulc,-1 -ln -selvar,$tracer $ds5day_file $tracer_file # -seltimestep,-20/-1 
+# tracer=BCU
+# tracer_file=$run_dir/dpscream_rce_large_3km_${run}_${tracer}_hrs.nc
+# cdo -mulc,-1 -ln -selvar,$tracer $ds5day_file $tracer_file # -seltimestep,-20/-1 
 
-tracer=NUC
-tracer_file=$run_dir/dpscream_rce_large_3km_${run}_${tracer}_hrs.nc
-cdo -mulc,-1 -ln -selvar,$tracer $ds5day_file $tracer_file # -seltimestep,-20/-1
+# tracer=NUC
+# tracer_file=$run_dir/dpscream_rce_large_3km_${run}_${tracer}_hrs.nc
+# cdo -mulc,-1 -ln -selvar,$tracer $ds5day_file $tracer_file # -seltimestep,-20/-1
 
-tracer=W_NUC
-tracer_file=$run_dir/dpscream_rce_large_3km_${run}_${tracer}_hrs.nc
-cdo -setname,$tracer -div -selvar,$tracer $ds5day_file -selvar,NUC $ds5day_file ${tracer_file}
+# tracer=W_NUC
+# tracer_file=$run_dir/dpscream_rce_large_3km_${run}_${tracer}_hrs.nc
+# cdo -setname,$tracer -div -selvar,$tracer $ds5day_file -selvar,NUC $ds5day_file ${tracer_file}
 
-tracer=NI_NUC
-tracer_file=$run_dir/dpscream_rce_large_3km_${run}_${tracer}_hrs.nc
-cdo -setname,$tracer -div -selvar,$tracer $ds5day_file -selvar,NUC $ds5day_file ${tracer_file}
+# tracer=NI_NUC
+# tracer_file=$run_dir/dpscream_rce_large_3km_${run}_${tracer}_hrs.nc
+# cdo -setname,$tracer -div -selvar,$tracer $ds5day_file -selvar,NUC $ds5day_file ${tracer_file}
 
 echo "done"
